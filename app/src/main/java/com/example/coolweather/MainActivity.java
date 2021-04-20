c0031d30e403a106d4af66ff4937d3fefe0377b6
bogdaniancu <bogdan.iancu@ymail.com>
2021-04-20 11:22:01 +0300

    Added AsyncTask

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -5,34 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.coolweather.models.City;
+import com.example.coolweather.models.WeatherCondition;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
     private static final int ADD_CITY_REQ_CODE = 1;
+
+    private TextView temperatureTextView = null;
+    private TextView conditionsTextView = null;
+    private ImageView imageView = null;
 
     private List<City> cities = new ArrayList<>();
     private Spinner spinnerLocation = null;
@@ -46,12 +61,11 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "Aplicatia a pornit!", Toast.LENGTH_LONG).show();
 
         Button btnAbout = findViewById(R.id.buttonAbout);
-        btnAbout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"DMC 2021", Toast.LENGTH_LONG).show();
-            }
-        });
+        btnAbout.setOnClickListener(v -> Toast.makeText(getApplicationContext(),"DMC 2021", Toast.LENGTH_LONG).show());
+
+        temperatureTextView = findViewById(R.id.textViewTemperature);
+        conditionsTextView = findViewById(R.id.textViewConditions);
+        imageView = findViewById(R.id.imageView);
 
 //        try {
 //            FileInputStream file = openFileInput("cities.dat");
@@ -80,6 +94,21 @@ public class MainActivity extends AppCompatActivity {
         spinnerLocation = findViewById(R.id.spinnerLocation);
         adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, cities);
         spinnerLocation.setAdapter(adapter);
+
+        spinnerLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinnerLocation != null && spinnerLocation.getSelectedItem() != null) {
+                    WeatherWorker worker = new WeatherWorker();
+                    worker.execute(spinnerLocation.getSelectedItem().toString());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
@@ -138,4 +167,62 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    class WeatherWorker extends AsyncTask<String, Void, WeatherCondition> {
+        private final static String API_KEY = "YOUR_API_KEY_HERE";
+
+        @Override
+        protected WeatherCondition doInBackground(String... strings) {
+            if(strings != null && strings.length > 0) {
+                String city = strings[0];
+                try {
+                    URL url = new URL(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&format=json&lang=ro",
+                            city, API_KEY));
+                    HttpsURLConnection connection =  (HttpsURLConnection) url.openConnection();
+                    InputStream inputStream = connection.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(reader);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = null;
+                    while((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    bufferedReader.close();
+                    String jsonText = stringBuilder.toString();
+                    JSONObject json = new JSONObject(jsonText);
+                    WeatherCondition condition = new WeatherCondition();
+                    JSONArray weatherArray = json.getJSONArray("weather");
+                    JSONObject weatherObject = weatherArray.getJSONObject(0);
+                    condition.setDescription(weatherObject.getString("description"));
+                    String icon = weatherObject.getString("icon");
+                    JSONObject mainObject = json.getJSONObject("main");
+                    condition.setTemperature((int)mainObject.getDouble("temp"));
+                    condition.setDate(new Date());
+
+                    url = new URL(String.format("https://openweathermap.org/img/wn/%s@4x.png", icon));
+                    connection = (HttpsURLConnection)url.openConnection();
+                    Bitmap img = BitmapFactory.decodeStream(connection.getInputStream());
+                    condition.setImage(img);
+
+                    return condition;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(WeatherCondition weatherCondition) {
+            super.onPostExecute(weatherCondition);
+            conditionsTextView.setText(weatherCondition.getDescription());
+            temperatureTextView.setText(String.format("%d° C", weatherCondition.getTemperature()));
+            imageView.setImageBitmap(weatherCondition.getImage());
+        }
+    }
 }
+
